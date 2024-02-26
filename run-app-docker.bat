23fac750baee4b4d9f228ef758a27b1105f77774
Andrew <mr.andrey.kozyrev@gmail.com>
2024-02-26 18:31:23 +0300

    - modified script

--- run-app-docker.bat
+++ run-app-docker.bat
@@ -1,20 +1,30 @@
 @echo off
 
+:: Check for existing image
 if not exist "docker image ls | findstr /i "app-image"" (
   echo Building image app-image...
 
+  :: Check for JAR file
   if not exist ".\\target\\students-console-app.jar" (
     echo Jar file not found, building project...
     mvnw.cmd clean package
   )
 
   docker build -t app-image .
-)
 
-echo Starting container...
-docker run -it --rm ^
-  -e SPRING_CONFIG_LOCATION=/config/application.yml ^
-  -v "%~dp0\app\application.yml:/config/application.yml:ro" ^
-  app-image %*
+  :: Start the container after image creation
+  echo Starting container...
+  docker run -it --rm ^
+    -e SPRING_CONFIG_LOCATION=/config/application.yml ^
+    -v "%~dp0\app\application.yml:/config/application.yml:ro" ^
+    app-image %*
+) else (
+  :: Image exists, start the container directly
+  echo Starting container...
+  docker run -it --rm ^
+    -e SPRING_CONFIG_LOCATION=/config/application.yml ^
+    -v "%~dp0\app\application.yml:/config/application.yml:ro" ^
+    app-image %*
+)
 
 PAUSE
